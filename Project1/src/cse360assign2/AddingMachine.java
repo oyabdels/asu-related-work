00822cecb15e9d3bb9bf83345a59321bad93d37d
yoabd <yoabd@10.157.247.11>
2019-10-09 11:09:15 -0700

    AddingMachine / Git testing

---
+++ Project1/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,57 @@
+/**
+ * Class that adds and divides numbers.
+ * @author Omer Abdelsalam
+ * @version 1.0
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * Constructor for the class. Sets the total to zero.
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the current value of the total.
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method adds the parameter value to the total variable.
+	 * @param value Integer parameter that gets added to the total.
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method subtracts the parameter value to the total variable.
+	 * @param value Integer paramtere that gets subtracted to the total.
+	 */
+	public void subtract (int value) {
+		
+	}
+		
+	/**
+	 * toString method that converts the object into a string.
+	 * @return Returns a string.
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * Clears the adding machine object. Sets total to 0.
+	 */
+	public void clear() {
+	
+	}
+}
